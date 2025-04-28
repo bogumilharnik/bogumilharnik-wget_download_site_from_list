866b8d29987b4b9e7a656e7f71e4ea12b5c210c6
bogumilharnik <117505733+bogumilharnik@users.noreply.github.com>
2025-04-28 14:57:27 +0200

    zmiana --convert-links na -K

--- backup_httrack_final.sh
+++ backup_httrack_final.sh
@@ -118,7 +118,7 @@ printf "%s\n" "${DOMENY[@]}" | parallel --env KATALOG_BACKUPU --env NIEUDANE --e
             -c8 \
             --connection-per-second=5 \
             --retries=2 \
-            --convert-links \
+            -K \
             -N1 \
             -sN \
             -z
